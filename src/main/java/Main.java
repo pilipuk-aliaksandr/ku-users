a7b35daf86610e67e51d6261b0c34798cb350e73
pilipuk-aliaksandr <shurapilipuk@gmail.com>
2025-01-18 15:50:18 +0300

    second lesson (map)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,29 +15,40 @@ public class Main {
         user1.setSurname("Pilipuk");
         user1.setAge(24);
         user1.setGender("Male");
+        user1.setCountry("Belarus");
 
         User user2 = new User();
         user2.setName("Veronica");
         user2.setSurname("Piun");
         user2.setAge(22);
         user2.setGender("Female");
+        user2.setCountry("Belarus");
 
         User user3 = new User();
         user3.setName("Veniamin");
         user3.setSurname("Plehanovich");
         user3.setAge(80);
         user3.setGender("Male");
+        user3.setCountry("Russia");
 
         list.add(user1);
         list.add(user2);
         list.add(user3);
 
-        int ageFilterParametr = UserService.ageFilterParameter();
-        String genderFilterParametr = UserService.genderFilterParameter();
+        int ageFilterParameter = UserService.ageFilterParameter();
+        String genderFilterParameter = UserService.genderFilterParameter();
 
-        System.out.println("This program works on forEach cycle: " + UserService.filterUsersWithForEach(list, ageFilterParametr, genderFilterParametr));
-        System.out.println("This program works on fori cycle: " + UserService.filterUsersWithFori(list, ageFilterParametr, genderFilterParametr));
-        System.out.println("This program works on while cycle: " + UserService.filterUsersWithWhile(list, ageFilterParametr, genderFilterParametr));
-        System.out.println("This program works on stream: " + UserService.filterUsersWithStream(list, ageFilterParametr, genderFilterParametr));
+        // The result of the zeroJavaLab Lesson1
+        System.out.println("This program works on forEach cycle: " + UserService.filterUsersWithForEach(list, ageFilterParameter, genderFilterParameter));
+        System.out.println("This program works on fori cycle: " + UserService.filterUsersWithFori(list, ageFilterParameter, genderFilterParameter));
+        System.out.println("This program works on while cycle: " + UserService.filterUsersWithWhile(list, ageFilterParameter, genderFilterParameter));
+        System.out.println("This program works on stream: " + UserService.filterUsersWithStream(list, ageFilterParameter, genderFilterParameter));
+
+        // The boarder (enter) between Lesson1 and Lesson2
+        System.out.println();
+
+        // The result of the zeroJavaLab Lesson2
+        System.out.println(UserService.mapUsersByCountry(list));
+        System.out.println(UserService.mapUsersByGender(list));
     }
 }
